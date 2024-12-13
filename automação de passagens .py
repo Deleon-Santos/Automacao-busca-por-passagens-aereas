0df34d9eb55a3bf024511c266a8f3f2645875e56
Deleon <delps.santos1987@gmail.com>
2024-12-13 20:29:05 -0300

    ajuste de regras em busca

--- automação de passagens .py
+++ automação de passagens .py
@@ -1,10 +1,12 @@
+"""Esta aplicação atomatiza um processo de pesquisa por passagens aereas, o projeto foi copiado do canal hashtag e otimizado para 
+para correção de erros, incersão de multiplas entradas e armazenamento em doc.txt"""
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-lista_companias = ["yR1fYc"]
+lista_companias = ['yR1fYc','A8qKrc','uVdL1c','uj4xv']
 
 navegador = webdriver.Chrome()
 
@@ -16,13 +18,16 @@ time.sleep(3)
 for classe in lista_companias:
     try:
         elemento = navegador.find_element(By.CLASS_NAME, classe).text 
-        elemento =elemento.replace('é',"e").replace('õ',"o").replace('-',"a")  
-        print(f" {elemento}")
-    
+        elemento =elemento.replace('é',"e").replace('õ',"o").replace(' – ',"a").replace(' – ',"a") 
+        print(f"\nClasse '{classe}' encontrada:\n {elemento}")
+      
+        
     except NoSuchElementException:
         print(f"Classe {classe} não encontrada.")
 
-with open("texto.txt","w") as arquivo:
+with open("texto.txt","a" ,encoding="utf-8") as arquivo:
     arquivo.write(elemento)
        
 
+#mxvQLc ceis6c uj4xv uVdL1c A8qKrc
+#mxvQLc ceis6c uj4xv uVdL1c A8qKrc
